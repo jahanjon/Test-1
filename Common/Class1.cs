e07a2f5742fc65c85b6d4c1f2ad768b58e6a06da
amir jahan <amirjahan675@gmail.com>
2024-10-31 13:38:55 +0330

    add ResultDto

---
+++ Common/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Common
+{
+
+    public class ResultDto<T>
+    {
+        public T Data { get; set; }
+
+        public MessageCodes MessageCode { get; set; }
+
+        public ResultDto()
+        {
+        }
+
+        public ResultDto(T data)
+        {
+            Data = data;
+            MessageCode = MessageCodes.Success;
+        }
+
+        public ResultDto(T data, MessageCodes messageCode)
+        {
+            Data = data;
+            MessageCode = messageCode;
+        }
+    }
+
+}
---
+++ Common/Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public enum MessageCodes
+    {
+        Success = 0,
+        DbConnectionError = 1,
+        UnHandleException = 2,
+        CaptchaNotValid = 3,
+        VerificationCodeNotValid = 4,
+        BadRequest = 5,
+        Unauthorized = 6,
+        CannotSendEmail = 7,
+        PaymentSuccess = 100,
+        PaymentFailure = 101,
+        AlreadyPaid = 102,
+        CanNotFindOrder = 103,
+        NoConnection = 104,
+        NoOrder = 105,
+        ThisPaymentHasAlreadyBeenProcessed = 106,
+        WeAreUnableToProcessYourPayment = 107,
+        CompanyNameIsRequired = 200,
+        VatIdIsNotValid = 201,
+        VatIdIsNotMatch = 202
+    }
+}
